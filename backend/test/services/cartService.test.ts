3aa7df1c0861f7881ba95555e76ac49964179d1e
Mohamed-Saber97 <mohamedkhaledsaber97@gmail.com>
2026-03-07 02:56:44 +0200

    prepare for testing

---
+++ backend/test/services/cartService.test.ts
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+import { addItemToCart } from "../../src/services/cartServices";
+import productModel from "../../src/models/productModel";
+import cartModel from "../../src/models/cartModel";
+
+describe("addItemTocart", ()=> {
+
+});
